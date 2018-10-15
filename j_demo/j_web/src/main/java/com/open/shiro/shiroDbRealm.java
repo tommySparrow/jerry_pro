6346b7c9af4f3501c7eca05c55660ca3146baff7
jerry <jm90_yang@163.com>
2018-10-15 09:40:48 +0800

    shiro的简单梳理,增加realm连接数据库

---
+++ j_demo/j_web/src/main/java/com/open/shiro/shiroDbRealm.java
@@ -0,0 +1,35 @@
+package com.open.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @ Author     ：jmyang
+ * @ Date       ：Created in 2018/10/12
+ * @ Description：
+ * @ throws
+ */
+public class shiroDbRealm extends AuthorizingRealm {
+
+    /**
+     * 认证
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+        return null;
+    }
+
+
+    /**
+     * 授权
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+}
